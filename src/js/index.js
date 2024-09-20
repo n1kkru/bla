9e5ac18dc9c7e4ecec64d050aac55f006a8fee31
Alex K <alex@dterra.ru>
2024-09-20 14:03:10 +0300

    minor fix

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ import 'virtual:svg-icons-register'
 import 'lazysizes'
 import config from './config'
 import cssDebug from './libs/css-debug/css-debug'
-import { barbaUi } from './libs/barba/barba'
+// import { barbaUi } from './libs/barba/barba'
 import { lenisInit } from './libs/lenis/lenis'
 import { scrollBarWidth } from './utils/scrollbarWidth'
 import { remSelect } from '../pug/shared/_ui-rem/rem-select/rem-select'
@@ -13,15 +13,17 @@ window.addEventListener('load', scrollBarWidth, false)
 window.addEventListener('resize', scrollBarWidth, false)
 
 document.addEventListener('DOMContentLoaded', function () {
-	// barbaUi()
-	commonFunction()
+  // barbaUi()
+  commonFunction()
 })
 
 export const commonFunction = () => {
-	// libs config
-	config()
-	cssDebug(true)
+  // libs config
+  config()
+  cssDebug(true)
 
-	// libs
-	lenisInit()
+  // libs
+  lenisInit()
+
+  console.log(1)
 }
--- src/js/libs/barba/barba.js
+++ src/js/libs/barba/barba.js
@@ -8,123 +8,123 @@ const transitionScreenActiveClass = '--active'
 const transitionScreenDelay = 1000
 
 export const barbaUi = () => {
-	if (barba.initialized) return // Проверка, чтобы избежать повторной инициализации
+  if (barba.initialized) return // Проверка, чтобы избежать повторной инициализации
 
-	// FRONT: Сброс скролла после завершения анимации перехода
-	barba.hooks.afterLeave(() => {
-		setTimeout(() => {
-			enableScroll() // Восстановление скролла на странице
-			window.scrollTo(0, 0)
-		}, transitionScreenDelay)
-	})
+  // FRONT: Сброс скролла после завершения анимации перехода
+  barba.hooks.afterLeave(() => {
+    setTimeout(() => {
+      enableScroll() // Восстановление скролла на странице
+      window.scrollTo(0, 0)
+    }, transitionScreenDelay)
+  })
 
-	// FRONT:  //BACK: при загрузке новой страницы все функции перезапускаются
-	// barba.hooks.afterEnter(() => {
-	// });
+  // FRONT:  //BACK: при загрузке новой страницы все функции перезапускаются
+  // barba.hooks.afterEnter(() => {
+  // });
 
-	barba.hooks.afterEnter(data => {
-		commonFunction() // Вызов функции при переходе между страницами
-		disableScroll() // Остановка скролла на странице
-	})
+  barba.hooks.afterEnter(data => {
+    commonFunction() // Вызов функции при переходе между страницами
+    disableScroll() // Остановка скролла на странице
+  })
 
-	// FRONT: Если очень быстрая прогрузка страницы и анимация не успевает проигрывать делаем задержку
-	const afterTransition = () => {
-		return new Promise(resolve => {
-			setTimeout(() => {
-				document.querySelector(transitionScreenID)?.classList.remove(transitionScreenActiveClass)
-				resolve()
-			}, transitionScreenDelay)
-		})
-	}
+  // FRONT: Если очень быстрая прогрузка страницы и анимация не успевает проигрывать делаем задержку
+  const afterTransition = () => {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        document.querySelector(transitionScreenID)?.classList.remove(transitionScreenActiveClass)
+        resolve()
+      }, transitionScreenDelay)
+    })
+  }
 
-	// BACK: если нам нужно программно перенаправить на страницу без анимаций (по дефолту) (все равно что изменить location.href)
-	// barba.force(href: string)
+  // BACK: если нам нужно программно перенаправить на страницу без анимаций (по дефолту) (все равно что изменить location.href)
+  // barba.force(href: string)
 
-	const urlParameters = new URLSearchParams(window.location.search)
-	if (urlParameters.has('test')) {
-		// в GET-параметре есть значение test
-		if (barba) {
-			// Barba.js инициализирован
-			barba.destroy()
-		}
-	} else {
-		// в GET-параметре нет значения test
-		// инициализация Barba.js
+  const urlParameters = new URLSearchParams(window.location.search)
+  if (urlParameters.has('test')) {
+    // в GET-параметре есть значение test
+    if (barba) {
+      // Barba.js инициализирован
+      barba.destroy()
+    }
+  } else {
+    // в GET-параметре нет значения test
+    // инициализация Barba.js
 
-		barba.init({
-			// BACK: По умолчанию мы кешируем страницы, если нужно добавить игнор
-			// cacheIgnore: ['/contact/', '/:category/post?'],
+    barba.init({
+      // BACK: По умолчанию мы кешируем страницы, если нужно добавить игнор
+      // cacheIgnore: ['/contact/', '/:category/post?'],
 
-			// BACK: по умолчанию включена предзагрузка стр при наведении на ссылку мышью, для игнора прописываем тут
-			// prefetchIgnore: '/home/'
+      // BACK: по умолчанию включена предзагрузка стр при наведении на ссылку мышью, для игнора прописываем тут
+      // prefetchIgnore: '/home/'
 
-			preventRunning: true,
+      preventRunning: true,
 
-			// BACK: при получении конкретной ошибки перекидываем на нашу стр 404
-			// requestError: (trigger, action, url, response) => {
-			//   if (action === 'click' && response.status && response.status === 404) {
-			//     barba.go('/404');
-			//   }
-			//   return false;
-			// },
+      // BACK: при получении конкретной ошибки перекидываем на нашу стр 404
+      // requestError: (trigger, action, url, response) => {
+      //   if (action === 'click' && response.status && response.status === 404) {
+      //     barba.go('/404');
+      //   }
+      //   return false;
+      // },
 
-			// hooks: {
-			//   afterEnter(data) {
-			//     console.info(234);
-			//     onloadCallback();
-			//   },
-			// },
+      // hooks: {
+      //   afterEnter(data) {
+      //     console.info(234);
+      //     onloadCallback();
+      //   },
+      // },
 
-			// FRONT: отображение доп информации для отладки
-			debug: true,
+      // FRONT: отображение доп информации для отладки
+      debug: true,
 
-			// FRONT: включаем эту настройку, если у нас переходящий экран не закрывает все пространство и пользователь может кликнуть во время загрузки другую ссылку Эта настройка предотвратит загрузку
-			// preventRunning: true,
+      // FRONT: включаем эту настройку, если у нас переходящий экран не закрывает все пространство и пользователь может кликнуть во время загрузки другую ссылку Эта настройка предотвратит загрузку
+      // preventRunning: true,
 
-			// FRONT: роутинг по страницам, namespace соответствует названию страницы
-			views: [
-				{
-					namespace: 'home',
+      // FRONT: роутинг по страницам, namespace соответствует названию страницы
+      views: [
+        {
+          namespace: 'home',
 
-					// FRONT:сюда вставляем функции которые нужны конкретно для этой страницы, всегда нужно вставлять те функции которые что-то переинициализируют, высчитывают скролл и т.д.
-					afterEnter() {},
+          // FRONT:сюда вставляем функции которые нужны конкретно для этой страницы, всегда нужно вставлять те функции которые что-то переинициализируют, высчитывают скролл и т.д.
+          afterEnter() { },
 
-					// FRONT: при выходе со стр убираем
+          // FRONT: при выходе со стр убираем
 
-					beforeLeave() {}
-				}
-			],
+          beforeLeave() { }
+        }
+      ],
 
-			// FRONT: Анимация перехода
-			// Массив, в него можно добавлять несколько кастомных транзишенов
-			transitions: [
-				{
-					name: 'default-transitions',
+      // FRONT: Анимация перехода
+      // Массив, в него можно добавлять несколько кастомных транзишенов
+      transitions: [
+        {
+          name: 'default-transitions',
 
-					leave(data) {
-						document.querySelector(transitionScreenID)?.classList.add(transitionScreenActiveClass)
-					},
+          leave(data) {
+            document.querySelector(transitionScreenID)?.classList.add(transitionScreenActiveClass)
+          },
 
-					afterEnter(data) {
-						return afterTransition()
-					}
-				}
-				// FRONT: Анимация которая сработает при переходе с определенной страницы на другую определенную страницу
+          afterEnter(data) {
+            return afterTransition()
+          }
+        }
+        // FRONT: Анимация которая сработает при переходе с определенной страницы на другую определенную страницу
 
-				// {
-				//   name: 'main-to-about', // специальная анимация для перехода между "main" и "about"
-				//   from: 'main',
-				//   to: 'about',
-				//   leave(data) {
-				//     // анимация при уходе со страницы main
-				//   },
-				//   enter(data) {
-				//     // анимация при входе на страницу about
-				//   },
-				// },
-			]
-		})
-	}
+        // {
+        //   name: 'main-to-about', // специальная анимация для перехода между "main" и "about"
+        //   from: 'main',
+        //   to: 'about',
+        //   leave(data) {
+        //     // анимация при уходе со страницы main
+        //   },
+        //   enter(data) {
+        //     // анимация при входе на страницу about
+        //   },
+        // },
+      ]
+    })
+  }
 
-	barba.initialized = true // Флаг, чтобы предотвратить повторную инициализацию
+  barba.initialized = true // Флаг, чтобы предотвратить повторную инициализацию
 }
