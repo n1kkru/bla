9d3f6039f99fe8ce4f13a0447b2357ceec982c66
Alex K <alex@dterra.ru>
2024-09-23 09:29:00 +0300

    upd scripts compile

--- vite.config.js
+++ vite.config.js
@@ -13,78 +13,78 @@ const root = resolve(path.dirname(url.fileURLToPath(import.meta.url)), 'src')
 const outDir = resolve(path.dirname(url.fileURLToPath(import.meta.url)), 'dist')
 
 export default defineConfig({
-	root,
-	base: './',
-	clearScreen: false,
-	build: {
-		outDir,
-		emptyOutDir: true,
-		minify: 'terser',
-		chunkSizeWarningLimit: '1024',
-		assetsInlineLimit: 0,
-		rollupOptions: {
-			output: {
-				assetFileNames: assetInfo => {
-					let extType = assetInfo.name.split('.')[1]
-					if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-						extType = 'images'
-					} else if (extType === 'css') {
-						extType = 'styles'
-					}
-					return `${extType}/[name][extname]`
-				},
-				chunkFileNames: 'scripts/scripts.js'
-			}
-		}
-	},
-	plugins: [
-		viteMultipage({
-			mimeCheck: true,
-			open: '/',
-			pageDir: 'pages',
-			purgeDir: 'pages',
-			removePageDirs: true,
-			rootPage: 'index.html'
-		}),
-		vitePug({
-			pugOptions: {
-				pretty: true
-			}
-		}),
-		viteEslint({
-			failOnError: false
-		}),
-		viteStylelint(),
-		viteSassGlob(),
-		viteImagemin({
-			gifsicle: {
-				optimizationLevel: 7,
-				interlaced: false
-			},
-			mozjpeg: {
-				quality: 75
-			},
-			pngquant: {
-				quality: [0.7, 0.7],
-				speed: 4
-			},
-			svgo: {
-				plugins: [
-					{
-						name: 'removeViewBox'
-					},
-					{
-						name: 'removeEmptyAttrs',
-						active: false
-					}
-				]
-			}
-		}),
-		createSvgIconsPlugin({
-			iconDirs: [resolve(process.cwd(), 'src/images')],
-			symbolId: '[name]',
-			inject: 'body-last',
-			customDomId: '__svg__icons__dom__'
-		})
-	]
+  root,
+  base: './',
+  clearScreen: false,
+  build: {
+    outDir,
+    emptyOutDir: true,
+    minify: 'terser',
+    chunkSizeWarningLimit: '1024',
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        assetFileNames: assetInfo => {
+          let extType = assetInfo.name.split('.')[1]
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'images'
+          } else if (extType === 'css') {
+            extType = 'styles'
+          }
+          return `${extType}/[name][extname]`
+        },
+        chunkFileNames: 'scripts/[name]-[hash].js'
+      }
+    }
+  },
+  plugins: [
+    viteMultipage({
+      mimeCheck: true,
+      open: '/',
+      pageDir: 'pages',
+      purgeDir: 'pages',
+      removePageDirs: true,
+      rootPage: 'index.html'
+    }),
+    vitePug({
+      pugOptions: {
+        pretty: true
+      }
+    }),
+    viteEslint({
+      failOnError: false
+    }),
+    viteStylelint(),
+    viteSassGlob(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false
+      },
+      mozjpeg: {
+        quality: 75
+      },
+      pngquant: {
+        quality: [0.7, 0.7],
+        speed: 4
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox'
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false
+          }
+        ]
+      }
+    }),
+    createSvgIconsPlugin({
+      iconDirs: [resolve(process.cwd(), 'src/images')],
+      symbolId: '[name]',
+      inject: 'body-last',
+      customDomId: '__svg__icons__dom__'
+    })
+  ]
 })
