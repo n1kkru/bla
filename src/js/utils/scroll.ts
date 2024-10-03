d6a832e8df5a21c2f453fe966c33a98ebeecd924
Knyazew36 <knyazew.sv@gmail.com>
2024-10-03 13:10:18 +0300

    test

--- src/js/utils/scroll.ts
+++ src/js/utils/scroll.ts
@@ -3,15 +3,13 @@ import { resumeLenis, stopLenis } from '../libs/lenis/lenis'
 import { isSafariFunction } from './isSafari'
 
 const isSafari = isSafariFunction()
-
-export function disableScroll() {
+              export function disableScroll() {
   document.querySelector('body')!.classList.add('overflow-scroll')
 
   if (window.screen.width > BREAKPOINT_DESKTOP && !isSafari) {
     stopLenis()
   }
 }
-
 export function enableScroll() {
   document.querySelector('body')!.classList.remove('overflow-hidden')
   if (window.screen.width > BREAKPOINT_DESKTOP && !isSafari) {
