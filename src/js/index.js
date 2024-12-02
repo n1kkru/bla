d235b63e4b49bdb5a2123ba16f4473043b993ed0
Gleb Shaev <shaevgleb@mail.ru>
2024-12-02 19:42:41 +0300

    update

--- src/js/index.js
+++ src/js/index.js
@@ -4,13 +4,13 @@ import 'virtual:svg-icons-register'
 
 import { remSelect } from '../pug/shared/_ui-rem/rem-select/rem-select'
 import '../styles/style.scss'
+import { validateFormInit } from './components/custom-validator'
 import config from './config'
 import { barbaUi } from './libs/barba/barba'
 import cssDebug from './libs/css-debug/css-debug'
 import { lenisInit } from './libs/lenis/lenis'
 import { scrollBarWidth } from './utils/scrollbarWidth'
 import { initSlideFunction } from './utils/slideFunction'
-import { validateFormInit } from './components/custom-validator'
 
 window.addEventListener('load', scrollBarWidth, false)
 window.addEventListener('resize', scrollBarWidth, false)
@@ -33,5 +33,5 @@ export const commonFunction = () => {
   // console.log(1)
 
   //Form
-  validateFormInit();
+  validateFormInit()
 }
